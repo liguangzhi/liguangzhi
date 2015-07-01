3f65cc04e8423adf845e719d1cda4264f3d8f8dd
Tarena250 <tarena250@xianfageiwo.local>
2015-07-01 11:28:13 +0800

    行的

---
+++ git444/git444/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git444
+//
+//  Created by Tarena250 on 15-7-1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git444/git444/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  git444
+//
+//  Created by Tarena250 on 15-7-1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"sfafsaf");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
